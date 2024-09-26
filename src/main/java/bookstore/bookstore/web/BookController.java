02457af687668b20ca6fda86c21511b5f29af0a7
Arxhaavi <artturihaavisto@hotmail.com>
2024-09-26 13:29:42 +0300

    first

---
+++ src/main/java/bookstore/bookstore/web/BookController.java
@@ -0,0 +1,20 @@
+package bookstore.bookstore.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import bookstore.bookstore.domain.Book;
+
+@ResponseBody
+@Controller
+public class BookController {
+     @GetMapping("/index")
+     
+     
+
+}
