b1ea3e07ce6b2c7ac23a1f5f7586855c7f7051f1
Arxhaavi <artturihaavisto@hotmail.com>
2024-10-24 15:47:26 +0300

    Add functionality added to database

--- src/main/java/bookstore/bookstore/web/BookController.java
+++ src/main/java/bookstore/bookstore/web/BookController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import bookstore.bookstore.domain.Book;
 import bookstore.bookstore.domain.BookRepository;
 
 @Controller
@@ -17,4 +20,15 @@ public class BookController {
           return "booklist";
      }
 
+     @RequestMapping("/add")
+     public String addBook(Model model) {
+          model.addAttribute("book", new Book());
+          return "addbook";
+     }
+
+     @RequestMapping(value = "/save", method = RequestMethod.POST)
+     public String save(Book book) {
+          bookRepository.save(book);
+          return "redirect:booklist";
+     }
 }
